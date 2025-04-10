ef38dcc42486a1368f9d473e5213afb7ff96ac87
NatalieGichuki <nataliegichuki@gmail.com>
2025-04-10 15:10:57 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import Article from "./Articlelist";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      
+      <Header name="My Amazing Blog"/>
+      
+      <About about="I am a trainee web developer who is currently blogging" 
+        image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"/>
+        <Article title="School Website" date="10th October 2025" preview="Let clean code guide you"/>
+        <Article title="Spa Business" date="24th August 2025" preview="Test on Javascript"/>
+        <Article title="The Argyle hotel" date="17th November 2025" preview="Home away from home"/>
     </div>
   );
 }
