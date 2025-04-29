4f68350bdd2bf6befcea29f5eafb1a01f6548116
NatalieGichuki <nataliegichuki@gmail.com>
2025-04-29 09:35:50 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,18 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
-import Article from "./Articlelist";
+import ArticleList from "./ArticleList"; 
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <Header name={blogData.name} />
+
+      <About about={blogData.about} image={blogData.image} />
       
-      <Header name="My Amazing Blog"/>
-      
-      <About about="I am a trainee web developer who is currently blogging" 
-        image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"/>
-        <Article title="School Website" date="10th October 2025" preview="Let clean code guide you"/>
-        <Article title="Spa Business" date="24th August 2025" preview="Test on Javascript"/>
-        <Article title="The Argyle hotel" date="17th November 2025" preview="Home away from home"/>
+      <ArticleList posts={blogData.posts} /> 
     </div>
   );
 }
